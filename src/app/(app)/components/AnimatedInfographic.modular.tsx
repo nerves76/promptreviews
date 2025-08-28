1d19c0779b4230d31c9893939aa3d702ddcdb56c
chris <59098887+nerves76@users.noreply.github.com>
2025-08-28 08:15:41 -0700

    Remove broken AnimatedInfographic files causing build errors

--- src/app/(app)/components/AnimatedInfographic.modular.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import React from 'react'
-
-// For now, just re-export the working monolithic version
-// TODO: Gradually replace with modular components
-export { default } from './AnimatedInfographic.with-animations'
